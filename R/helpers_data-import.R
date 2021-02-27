7deed4be2e26f7d96a856a82abf86f7516ab29b2
Luke <lukece@mailbox.org>
2021-02-27 20:48:17 +0100

    expolore identification with geographies

--- R/helpers_data-import.R
+++ R/helpers_data-import.R
@@ -8,7 +8,11 @@ get_raw_data_loaders <- function(){
     client_customers = "0010-1_tse-geomarketing-customers_bva.RData",
     iris_pop = "0002-1_iris-pop-2017_insee.RData",
     iris_income = "0004-1_iris-income-2017_insee.RData",
-    mun_income = "0018-1_fr-mun-income-2017_insee.RData") %>%
+    mun_income = "0018-1_fr-mun-income-2017_insee.RData",
+    iris_poly19 = "0001-1_iris-polygons-2019_ign.RData",
+    iris_poly16 = "0022-1_iris-polygons-2016_ign.RData",
+    iris_poly15 = "0023-1_iris-polygons-2015_ign.RData",
+    iris_poly14 = "0024-1_iris-polygons-2014_ign.RData") %>%
     lapply(FUN = function(s) paste0(raw_data_location,s))
 
 
